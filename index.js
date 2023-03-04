a7a94c3d05ceac609be799033f0d63b82d24d5da
Abdus Sattar <sasorganicagro@gmail.com>
2023-03-05 02:03:30 +0600

    Add CRUD functionality

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ const displayOnUI = () =>{
        <hr> `;
   });
   document.querySelector('.displayListCont').innerHTML = items;
+   editListener();
+   saveListener();
+   deleteListener();
+
 }
 
 /* generate Index for every toDoList */
@@ -51,6 +55,54 @@ const pushData = () => {
     displayOnUI();
   });
 };
+
+/* for the textarea  editable and show controllers(deleteBtn , saveBtn )*/
+const editListener = ()=>{
+  const textareas = document.querySelectorAll('textarea');
+const editBtns = document.querySelectorAll('.editBtn');
+const controllers = document.querySelectorAll('.controller');
+editBtns.forEach((editBtn,i) =>{
+  editBtn.addEventListener('click', ()=>{
+    controllers[i].style.display = 'flex';
+    editBtn.style.display = 'none';
+    textareas[i].disabled = false;
+  })
+})
+}
+
+/* for saving the textarea after editing */
+const saveListener = () =>{
+  const textareas = document.querySelectorAll('textarea');
+  const saveBtns = document.querySelectorAll('.saveBtn');
+   saveBtns.forEach((saveBtn, i) =>{
+    saveBtn.addEventListener('click',()=>{
+      /* after editing reassign the selected textarea value/task in data array  */
+     data[i].task = textareas[i].value;
+     setInLocal();
+     /* update UI and also disappear the controllers when call displayOnUI function */
+     displayOnUI();
+  })
+})
+}
+
+/* for deleting each item / textarea */
+
+const deleteListener = ()=>{
+  const deleteBtns = document.querySelectorAll('.deleteBtn');
+  deleteBtns.forEach((deleteBtn,i) =>{
+  deleteBtn.addEventListener('click',()=>{
+    /* After applying the filter method  get a filtered array,
+    in which no item remains that we want to delete. Then reassign the data array. */
+    data = data.filter((singleObj,index) => index !== i);
+    /* after deleting update the index of the each object */
+    data.forEach((singleObj,i)=>{
+      singleObj.index =i;
+    })
+    setInLocal();
+    displayOnUI()
+  })
+})
+}
 
 retrieveFromLocal();
 pushData();
